1d9df21bfbf9596788e8c14eafa909b376b9a96d
Leonardo Araújo <progamadorweb18@gmail.com>
2025-07-20 10:44:03 -0300

    Projeto Semáforo

--- semaforo/script.js
+++ semaforo/script.js
@@ -3,6 +3,8 @@ let red = document.getElementById("red");
 let yellow = document.getElementById("yellow");
 let green = document.getElementById("green");
 let automatic = document.getElementById("automatic");
+let intervalId = null;
+let tick = 0;
 
 function vermelho(){
   img.src = './imagem/vermelho.png'
@@ -17,7 +19,27 @@ function verde(){
 }
 
 function automatico(){
-    
+    clearInterval(intervalId);
+
+    tick = 0;
+    vermelho();
+
+    intervalId = setInterval (() => {
+      tick++
+
+      if(tick === 3){
+        amarelo()
+      }
+
+      if(tick === 4){
+        verde()
+      }
+
+      if(tick === 7){
+        vermelho()
+        tick = 0;
+      }
+    }, 500)
 }
 
 red.addEventListener("click", vermelho);
